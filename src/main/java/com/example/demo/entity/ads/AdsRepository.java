720f9fbc675521219b64e9e1efa4fdd13108e9e3
Bahodir <turayev.bahodir95@gmail.com>
2023-04-15 16:52:17 +0500

    hot fix

--- src/main/java/com/example/demo/entity/ads/AdsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.entity.ads;
-
-import com.example.demo.entity.Ads;
-import com.example.demo.filter.JpaGenericRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdsRepository extends JpaRepository<Ads, Long>, JpaGenericRepository<Ads> {
-}
--- src/main/java/com/example/demo/entity/user/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.entity.user;
-
-import com.example.demo.filter.JpaGenericRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long>, JpaGenericRepository<User> {
-}
